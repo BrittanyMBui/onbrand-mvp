741fad8b72e5f57c4d821e8980e676b5e9bb4c23
brittanymbui <brittymbui@gmail.com>
2021-06-18 18:36:54 -0700

    Adds links to navbar

--- onbrand-client/src/components/NavBar.js
+++ onbrand-client/src/components/NavBar.js
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink, NavbarText } from 'reactstrap';
 
 function NavBar() {
@@ -9,7 +8,16 @@ function NavBar() {
                 <NavbarBrand href='/'>On Brand</NavbarBrand>
                 <Nav className='mr-auto' navbar>
                     <NavItem>
+                        <NavLink href='/blog'>Blog</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href='/faqs'>FAQs</NavLink>
+                    </NavItem>
+                    <NavItem>
                         <NavLink href='/about'>About</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href='/login'>Login</NavLink>
                     </NavItem>
                 </Nav>
             </Navbar>
