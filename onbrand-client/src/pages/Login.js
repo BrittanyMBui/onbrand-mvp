b7d8e467a02e5df10cb91e9d4c007a3f26af4910
brittanymbui <brittymbui@gmail.com>
2021-06-18 19:56:28 -0700

    Adds fields to login page

--- onbrand-client/src/pages/Login.js
+++ onbrand-client/src/pages/Login.js
@@ -1,7 +1,18 @@
+import React from 'react';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 function Login() {
     return (
-        <h1>Login</h1>
+        <Form>
+            <FormGroup>
+                <Label for='email'>email</Label>
+                <Input type="email" name="email" id="email" placeholder="email..." />
+            </FormGroup>
+            <FormGroup>
+                <Label for="examplePassword">Password</Label>
+                <Input type="password" name="password" id="password" placeholder="password..." />
+            </FormGroup>
+        </Form>
     )
 }
 
