b918f67ddb61368b0a7b3086d5020b0999677b76
brittanymbui <brittymbui@gmail.com>
2021-06-18 19:40:12 -0700

    Adds login link/page to navbar

--- onbrand-client/src/App.js
+++ onbrand-client/src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 
 import Home from './pages/Home';
 import NavBar from './components/NavBar';
+import Login from './pages/Login';
 
 function App() {
   return (
     <Router>
       <NavBar />
       <Route exact path='/' component={Home} />
+      <Route exact path='/login' component={Login} />
     </Router>
   );
 }
