3ba667a0b30a3ca76a613433cf2bfb0e38f40732
brittanymbui <brittymbui@gmail.com>
2021-06-21 21:46:56 -0700

    Adds context to apollo client - in test

--- onbrand-client/src/ApolloProvider.js
+++ onbrand-client/src/ApolloProvider.js
@@ -1,9 +1,26 @@
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
+
 import App from './App';
 
+
+const httpLink = new HttpLink({
+    uri: 'http://localhost:4000'
+})
+
+const authLink = setContext(() => {
+    const token = localStorage.getItem('token');
+    return {
+        header : {
+            Authorization: token ? `Bearer ${token}` : ''
+        }
+    }
+})
+
 const client = new ApolloClient({
-uri: 'http://localhost:4000',
-cache: new InMemoryCache()
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
 });
 
 export default (
