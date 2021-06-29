bbfe900a842142ffa6e9286535dcd7950cd70817
brittanymbui <brittymbui@gmail.com>
2021-06-29 11:57:38 -0700

    Adds user's name/info to homepage

--- onbrand-client/src/pages/Home.js
+++ onbrand-client/src/pages/Home.js
@@ -1,9 +1,17 @@
 import React, { useContext } from 'react';
 import { useQuery } from '@apollo/client';
 
+import { AuthContext } from '../context/auth';
+
 function Home(props) {
+    const { user } = useContext(AuthContext);
+    console.log(user);
+
     return (
-        <h1>On Brand</h1>
+        <>
+        <h1>Welcome back, {user.name}</h1>
+        <p></p>
+        </>
     )
 }
 
